e039ee822d3d118f178632c695b44634740cf34d
杜小东 <duxiaodong@xuhuaqideiMac.local>
2015-06-02 18:15:52 +0800

    add

---
+++ README.md
@@ -0,0 +1,4 @@
+frameAnimate
+============
+
+canvas播放序列帧动画
---
+++ frameAnimate.js
@@ -0,0 +1,168 @@
+'use strict';
+
+function frameAnimate(canvas, opts, callback) {
+
+	this.canvas = canvas;
+
+	this.width = opts.width;
+	this.height = opts.height;
+	this.frame = opts.frame;
+	this.fps = opts.fps || 24;
+	this.loop = opts.loop;
+	this.loopCount = opts.loopCount;
+	this.singleMode = opts.singleMode || false;
+
+	this.callback = callback;
+
+	this._currentFrame = 0;
+	this.source = this.canvas.getAttribute('data-source');
+	this._setInterval;
+	this._onloadLength = 0;
+
+	this._frames = [];
+
+	this.init();
+
+}
+
+frameAnimate.prototype = {
+	init: function() {
+		var self = this;
+		this.canvas.width = this.width;
+		this.canvas.height = this.height;
+
+		this._ctx = this.canvas.getContext("2d");
+
+		if (this.singleMode) {
+		    singleMode();
+		} else {
+			picsMode();
+		}
+		// 未完成
+		function singleMode() {
+			self._img = new Image();
+		    self._img.src = self.source;
+		    self._img.onload = function() {
+		        var img_WIDTH = self._img.width / self.width;
+		        var img_HEIGHT = self._img.height / self.height;
+		        for (var i = 0; i < img_HEIGHT; i++) {
+		            for (var j = 0; j < img_WIDTH; j++) {
+		                var frame = {
+		                    x: (j % img_WIDTH) * self.width,
+		                    y: i * self.height
+		                };
+		                self._frames.push(frame);
+		            }
+		        }
+		    };
+		}
+
+		function picsMode() {
+			for (var i = 0; i < self.frame; i++) {
+				var _img = new Image();
+				_img.src = self.source + i + '.png';
+				self._frames.push(_img);
+
+				_img.onload = function() {
+					self._onloadLength++;
+				}
+			}
+		}
+
+	},
+	draw: function() {
+		var self = this;
+		self._ctx.clearRect(0, 0, self.width, self.height);
+		var f = self._frames[self._currentFrame];
+		if (self.singleMode) {
+			self._ctx.drawImage(self._img, f.x, f.y, self.width, self.height);
+		} else {
+			self._ctx.drawImage(f, 0, 0, self.width, self.height);
+		}
+		
+		if (self._currentFrame === self.frame - 1) {
+
+			self._currentFrame = 0;
+			if (!self.loop) {
+				// clearInterval(self._setInterval);
+				self.stop();
+			}
+			if (self.loopCount) {
+				if (self.loopCount > 1) {
+					self.loopCount--;
+				} else {
+					// clearInterval(self._setInterval);
+					self.stop();
+				}
+			}
+		} else {
+			self._currentFrame++;
+		}
+	},
+	// 图片加载检查
+	checkLoad: function(next, n) {
+		var self = this;
+		self._setInterval = setInterval(function() {
+			if (self._onloadLength === self.frame) {
+				// console.log("==pics load complete==");
+				clearInterval(self._setInterval);
+				
+				switch(next) {
+					case 'draw':
+						self.draw();
+						break;
+					case 'seekTo':
+						self.seekTo(n);
+						break;
+					default:
+						self.play();
+				}
+			}
+		}, 1000/this.fps);
+	},
+	play: function() {
+
+		var self = this;
+		self.pause();
+		if (self._onloadLength < self.frame) {
+			self.checkLoad('play');
+		} else {
+			self._setInterval = setInterval(function() {
+			if (self._onloadLength === self.frame) {
+					self.draw();
+				}
+			}, 1000/this.fps);
+		}
+		
+	},
+	pause: function() {
+		var self = this;
+		if (self._setInterval) {
+			clearInterval(self._setInterval);
+		}
+	},
+	stop: function() {
+		var self = this;
+		self.pause();
+		self.seekTo(self.frame - 1);
+		if (self.callback) {
+			self.callback();
+		}
+	},
+	replay: function() {
+		this.pause();
+		this.seekTo(0);
+		this.play();
+	},
+	seekTo: function(n) {
+		var self = this;
+
+		self.pause();
+		self._currentFrame = n;
+		if (self._onloadLength < self.frame) {
+			self.checkLoad('seekTo', n);
+		} else {
+			self.draw();
+		}
+	}
+};
