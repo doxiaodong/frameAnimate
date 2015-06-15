ffbb0abbf7c7067a514f73edf4a3ba0a06514d81
duxiaodong <duxiaodng@darlin.me>
2015-06-15 13:03:01 +0800

    optimize prototype

--- frameAnimate.js
+++ frameAnimate.js
@@ -25,144 +25,146 @@ function frameAnimate(canvas, opts, callback) {
 
 }
 
-frameAnimate.prototype = {
-	init: function() {
-		var self = this;
-		this.canvas.width = this.width;
-		this.canvas.height = this.height;
+frameAnimate.prototype.init = function() {
+	var self = this;
+	console.log(self)
+	this.canvas.width = this.width;
+	this.canvas.height = this.height;
 
-		this._ctx = this.canvas.getContext("2d");
+	this._ctx = this.canvas.getContext("2d");
 
-		if (this.singleMode) {
-		    singleMode();
-		} else {
-			picsMode();
-		}
-		// 未完成
-		function singleMode() {
-			self._img = new Image();
-		    self._img.src = self.source;
-		    self._img.onload = function() {
-		        var img_WIDTH = self._img.width / self.width;
-		        var img_HEIGHT = self._img.height / self.height;
-		        for (var i = 0; i < img_HEIGHT; i++) {
-		            for (var j = 0; j < img_WIDTH; j++) {
-		                var frame = {
-		                    x: (j % img_WIDTH) * self.width,
-		                    y: i * self.height
-		                };
-		                self._frames.push(frame);
-		            }
-		        }
-		    };
-		}
-
-		function picsMode() {
-			for (var i = 0; i < self.frame; i++) {
-				var _img = new Image();
-				_img.src = self.source + i + '.png';
-				self._frames.push(_img);
-
-				_img.onload = function() {
-					self._onloadLength++;
+	if (this.singleMode) {
+		singleMode();
+	} else {
+		picsMode();
+	}
+	// 未完成
+	function singleMode() {
+		self._img = new Image();
+		self._img.src = self.source;
+		self._img.onload = function() {
+			var img_WIDTH = self._img.width / self.width;
+			var img_HEIGHT = self._img.height / self.height;
+			for (var i = 0; i < img_HEIGHT; i++) {
+				for (var j = 0; j < img_WIDTH; j++) {
+					var frame = {
+						x: (j % img_WIDTH) * self.width,
+						y: i * self.height
+					};
+					self.frames.push(frame);
 				}
 			}
-		}
+		};
+	}
 
-	},
-	draw: function() {
-		var self = this;
-		self._ctx.clearRect(0, 0, self.width, self.height);
-		var f = self._frames[self._currentFrame];
-		if (self.singleMode) {
-			self._ctx.drawImage(self._img, f.x, f.y, self.width, self.height);
-		} else {
-			self._ctx.drawImage(f, 0, 0, self.width, self.height);
-		}
-		
-		if (self._currentFrame === self.frame - 1) {
+	function picsMode() {
+		for (var i = 0; i < self.frame; i++) {
+			var _img = new Image();
+			_img.src = self.source + i + '.png';
+			self._frames.push(_img);
 
-			self._currentFrame = 0;
-			if (!self.loop) {
+			_img.onload = function() {
+				self._onloadLength++;
+			}
+		}
+	}
+};
+
+frameAnimate.prototype.draw = function() {
+	var self = this;
+	self._ctx.clearRect(0, 0, self.width, self.height);
+	var f = self._frames[self._currentFrame];
+	if (self.singleMode) {
+		self._ctx.drawImage(self._img, f.x, f.y, self.width, self.height);
+	} else {
+		self._ctx.drawImage(f, 0, 0, self.width, self.height);
+	}
+
+	if (self._currentFrame === self.frame - 1) {
+
+		self._currentFrame = 0;
+		if (!self.loop) {
+			// clearInterval(self._setInterval);
+			self.pause();
+		}
+		if (self.loopCount) {
+			if (self.loopCount > 1) {
+				self.loopCount--;
+			} else {
 				// clearInterval(self._setInterval);
-				self.stop();
+				self.pause();
 			}
-			if (self.loopCount) {
-				if (self.loopCount > 1) {
-					self.loopCount--;
-				} else {
-					// clearInterval(self._setInterval);
-					self.stop();
-				}
-			}
-		} else {
-			self._currentFrame++;
 		}
-	},
-	// 图片加载检查
-	checkLoad: function(next, n) {
-		var self = this;
+	} else {
+		self._currentFrame++;
+	}
+};
+
+frameAnimate.prototype.checkLoad = function(next, n) {
+	var self = this;
+	self._setInterval = setInterval(function() {
+		if (self._onloadLength === self.frame) {
+			// console.log("==pics load complete==");
+			clearInterval(self._setInterval);
+
+			switch(next) {
+				case 'draw':
+					self.draw();
+					break;
+				case 'seekTo':
+					self.seekTo(n);
+					break;
+				default:
+					self.play();
+			}
+		}
+	}, 1000/this.fps);
+};
+
+frameAnimate.prototype.play = function() {
+	var self = this;
+	self.pause();
+	if (self._onloadLength < self.frame) {
+		self.checkLoad('play');
+	} else {
 		self._setInterval = setInterval(function() {
 			if (self._onloadLength === self.frame) {
-				// console.log("==pics load complete==");
-				clearInterval(self._setInterval);
-				
-				switch(next) {
-					case 'draw':
-						self.draw();
-						break;
-					case 'seekTo':
-						self.seekTo(n);
-						break;
-					default:
-						self.play();
-				}
+				self.draw();
 			}
 		}, 1000/this.fps);
-	},
-	play: function() {
+	}
+};
 
-		var self = this;
-		self.pause();
-		if (self._onloadLength < self.frame) {
-			self.checkLoad('play');
-		} else {
-			self._setInterval = setInterval(function() {
-			if (self._onloadLength === self.frame) {
-					self.draw();
-				}
-			}, 1000/this.fps);
-		}
-		
-	},
-	pause: function() {
-		var self = this;
-		if (self._setInterval) {
-			clearInterval(self._setInterval);
-		}
-	},
-	stop: function() {
-		var self = this;
-		self.pause();
-		self.seekTo(self.frame - 1);
-		if (self.callback) {
-			self.callback();
-		}
-	},
-	replay: function() {
-		this.pause();
-		this.seekTo(0);
-		this.play();
-	},
-	seekTo: function(n) {
-		var self = this;
+frameAnimate.prototype.pause = function() {
+	var self = this;
+	if (self._setInterval) {
+		clearInterval(self._setInterval);
+	}
+	if (self.callback) {
+		self.callback();
+	}
+};
 
-		self.pause();
-		self._currentFrame = n;
-		if (self._onloadLength < self.frame) {
-			self.checkLoad('seekTo', n);
-		} else {
-			self.draw();
-		}
+frameAnimate.prototype.stop = function() {
+	var self = this;
+	self.pause();
+	self.seekTo(self.frame - 1);
+};
+
+frameAnimate.prototype.replay = function() {
+	this.pause();
+	this.seekTo(0);
+	this.play();
+};
+
+frameAnimate.prototype.seekTo = function(n) {
+	var self = this;
+
+	self.pause();
+	self._currentFrame = n;
+	if (self._onloadLength < self.frame) {
+		self.checkLoad('seekTo', n);
+	} else {
+		self.draw();
 	}
 };
